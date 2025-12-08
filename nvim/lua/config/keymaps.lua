920adc087c5a4446ebdbcc87bcc2a2b982b24d18
sunanmau5 <sunan.regi@gmail.com>
2025-12-09 00:13:11 +0100

    feat: add keymaps for start and end of line navigation

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- normal mode
+map("n", "<S-h>", "^", { desc = "start of line" })
+map("n", "<S-l>", "$", { desc = "end of line" })
