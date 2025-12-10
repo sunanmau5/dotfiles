b94615978d1fc9fe032ab6bf5578689804ef983a
sunanmau5 <sunan.regi@gmail.com>
2025-12-10 14:15:51 +0100

    feat: add y shell wrapper to change cwd when exiting yazi

--- zsh/.config/zsh/functions.sh
+++ zsh/.config/zsh/functions.sh
@@ -5,3 +5,12 @@ function kl() {
 function g-sd() {
   git stash drop stash@{"${1:-0}"}
 }
+
+# yazi
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
