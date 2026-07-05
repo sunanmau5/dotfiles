157654d1a6780eb0d9239155cd604757a1c4e8a3
sunanmau5 <sunan.regi@gmail.com>
2026-07-05 12:28:17 +0200

    feat(zsh): remove autocd

--- zsh/.config/zsh/options.zsh
+++ zsh/.config/zsh/options.zsh
@@ -1,5 +1,4 @@
 setopt inc_append_history # share history across tmux panes/windows
-setopt hist_ignore_dups # skip consecutive duplicates
-setopt hist_ignore_space # skip commands prefixed with a space
-setopt autocd # cd into directories without typing cd
-setopt extendedglob # enable advanced glob patterns
+setopt hist_ignore_dups   # skip consecutive duplicates
+setopt hist_ignore_space  # skip commands prefixed with a space
+setopt extendedglob       # enable advanced glob patterns
